3b2cb4ab0d6b4649b05b9cb87f247106d8a0cb3b
Charles Martin <charles.martin@gmail.com>
2019-12-19 17:47:59 -0800

    Add parameter addressing modes

--- src/IntCode.hs
+++ src/IntCode.hs
@@ -9,21 +9,34 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
 type Output = Maybe (Int,[Int])
+data Addr = Pos | Imm deriving (Eq,Ord,Show)
+
+decode :: Int -> (Int,[Addr])
+decode x = (op, addr raw) where
+    (raw,op) = divMod x 100
+    addr r = bool Pos Imm (odd r) : addr (div r 10)
 
 intCode :: [Int] -> [Int] -> Output
 intCode code input = go 0 codemap input [] where
     codemap = M.fromList $ zip [0..] code
-    go c m i o = M.lookup c m >>= \case
-        99 -> (,o) <$> M.lookup 0 m
-        1 -> binop (+)
-        2 -> binop (*)
-        q -> Nothing
+    go c m i o = M.lookup c m >>= \opcode -> 
+        let (op,addr) = decode opcode in case op of
+            99 -> halt
+            1 -> binop (+) addr
+            2 -> binop (*) addr
+            q -> Nothing
       where
+        lookup = flip M.lookup m
+        address Imm = Just
+        address Pos = lookup
         get :: [Int] -> Maybe [Int]
-        get = mapM (flip M.lookup m)
-        binop :: (Int -> Int -> Int) -> Output
-        binop op = do
-            [x,y,r] <- get [c+1..c+3]
-            [a,b]   <- get [x,y]
+        get = mapM lookup
+        halt :: Output
+        halt = (,o) <$> lookup 0
+        binop :: (Int -> Int -> Int) -> [Addr] -> Output
+        binop op (x:y:_) = do
+            a <- lookup (c+1) >>= address x
+            b <- lookup (c+2) >>= address y
+            r <- lookup (c+3)
             let m' = M.insert r (op a b) m
             go (c+4) m' i o
