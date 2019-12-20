793b3c72faa27d92488d518e05742f116ccea711
Charles Martin <charles.martin@gmail.com>
2019-12-20 14:25:18 -0800

    Small refactor

--- src/Day07.hs
+++ src/Day07.hs
@@ -8,7 +8,7 @@ import IntCode
 
 day07 :: [Int] -> (Int,Int)
 day07 code = (part1,part2) where
-    part1 = maximum $ foldl' f 0 <$> permutations [0..4]
+    part1 = maximum $ foldl' f 0 <$> permutations [0..4] where 
+        f i p = o where
+            Just (_,(o:_)) = intCode code [p,i]
     part2 = 0
-    f i p = o where
-        Just (_,(o:_)) = intCode code [p,i]
