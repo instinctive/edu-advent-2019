81c83f5078845164fb9156911c0ba0df75049f8b
Charles Martin <charles.martin@gmail.com>
2019-12-21 13:46:19 -0800

    Refactor address modes, add explicit i/o parameters

--- src/IntCode.hs
+++ src/IntCode.hs
@@ -9,8 +9,9 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
 type Output = [Either (Maybe Int) Int]
-type Mode = Int -> Int
 type Op = [Int] -> Output
+data Param = I | O deriving (Eq,Ord,Show)
+data Mode = Pos | Imm | Rel deriving (Eq,Ord,Show)
 
 intCode :: [Int] -> [Int] -> Output
 intCode code input = go 0 0 codemap input where
@@ -18,35 +19,37 @@ intCode code input = go 0 0 codemap input where
     go base c m i = case op of
         99 -> halt
         0 -> error "zero opcode"
-        1 -> get' 2 addr $ binop (+)
-        2 -> get' 2 addr $ binop (*)
-        3 -> get' 0 addr $ read
-        4 -> get  1 addr $ write
-        5 -> get  2 addr $ jumpif id
-        6 -> get  2 addr $ jumpif not
-        7 -> get' 2 addr $ cmp (<)
-        8 -> get' 2 addr $ cmp (==)
-        9 -> get  1 addr $ offset
+        1 -> {- traceShow "binop+" $ -} get [I,I,O] $ binop (+)
+        2 -> {- traceShow "binop*" $ -} get [I,I,O] $ binop (*)
+        3 -> {- traceShow "read  " $ -} get [O]     $ read
+        4 -> {- traceShow "write " $ -} get [I]     $ write
+        5 -> {- traceShow "jump  " $ -} get [I,I]   $ jumpif id
+        6 -> {- traceShow "jump -" $ -} get [I,I]   $ jumpif not
+        7 -> {- traceShow "cmp lt" $ -} get [I,I,O] $ cmp (<)
+        8 -> {- traceShow "cmp eq" $ -} get [I,I,O] $ cmp (==)
+        9 -> {- traceShow "offset" $ -} get [I]     $ offset
         q -> error $ printf "invalid opcode: %d" q
       where
         op :: Int
         addr :: [Mode]
         (op,addr) = second f (swap $ divMod (lookup c) 100) where
-            f modes = let (modes',mode) = divMod modes 10 in mk mode : f modes'
-            mk 0 = pos
-            mk 1 = imm
-            mk 2 = rel
+            f modes = uncurry (:) . bimap mk f . swap $ divMod modes 10
+            mk 0 = Pos; mk 1 = Imm; mk 2 = Rel
             mk q = error $ printf "invalid address mode: %d" q
         lookup :: Int -> Int
         lookup i | i < 0 = error $ printf "negative address: %d" i
         lookup i = M.findWithDefault 0 i m
-        imm,pos,rel :: Mode
-        imm = lookup
-        pos = lookup . lookup
-        rel = lookup . (+base) . lookup
-        get',get :: Int -> [Mode] -> Op -> Output
-        get' n addr = get (n+1) (take n addr ++ [imm])
-        get  n addr f = f $ zipWith ($) addr [c+1..c+n]
+        get :: [Param] -> Op -> Output
+        get pp f = 
+            -- traceShow (zip pp addr) $
+            f $ zipWith3 mk pp addr [c+1..]
+          where
+            mk I Imm = lookup
+            mk I Pos = lookup . lookup
+            mk I Rel = lookup . (+base) . lookup
+            mk O Imm = error "immediate mode for output parameter"
+            mk O Pos = lookup
+            mk O Rel = (+base) . lookup
         halt :: Output
         halt = [Left . Just $ lookup 0] -- for Day02
         binop :: (Int -> Int -> Int) -> Op
