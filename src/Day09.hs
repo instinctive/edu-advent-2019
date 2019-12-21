7c457616b1de307693bc4789b61148dfa1e99b3e
Charles Martin <charles.martin@gmail.com>
2019-12-21 13:48:51 -0800

    Solution for Day09

--- src/Day09.hs
+++ src/Day09.hs
@@ -9,6 +9,4 @@ import IntCode
 day09 :: [Int] -> (Int,Int)
 day09 code = (part1,part2) where
     part1 = head . rights $ intCode code [1]
-    part2 = 0
-    -- part2 = head . rights $ intCode code [5]
-
+    part2 = head . rights $ intCode code [2]
